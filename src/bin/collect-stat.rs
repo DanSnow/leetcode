1855e1463f26fa9ec455db57ad740788a4ec991c
DanSnow <dododavid006@gmail.com>
2024-06-06 19:01:37 +0800

    chore: add stat

---
+++ src/bin/collect-stat.rs
@@ -0,0 +1,98 @@
+use std::{
+    collections::BTreeMap,
+    fs::{self, File},
+    io::Write,
+    os::unix::ffi::OsStrExt,
+};
+
+static TARGET_DIRS: [&str; 3] = ["easy", "medium", "hard"];
+
+#[derive(Clone, Debug)]
+struct Report {
+    easy: BTreeMap<String, i32>,
+    medium: BTreeMap<String, i32>,
+    hard: BTreeMap<String, i32>,
+}
+
+fn main() {
+    fs::create_dir_all(".stat").expect("fail to create dir");
+    let db = sled::open(".stat/db").expect("fail to create db");
+
+    let mut report = Report {
+        easy: BTreeMap::new(),
+        hard: BTreeMap::new(),
+        medium: BTreeMap::new(),
+    };
+
+    for dir in TARGET_DIRS {
+        if let Ok(entries) = fs::read_dir(format!("src/{dir}")) {
+            for entry in entries {
+                let entry = entry.unwrap();
+                let file_name = entry.file_name();
+                let filename = file_name.as_bytes();
+                if filename == b"mod.rs" {
+                    continue;
+                }
+                let value = db.get(filename).expect("fail to get key");
+                let date = match value {
+                    Some(value) => {
+                        String::from_utf8(value.to_vec()).expect("fail to convert to string")
+                    }
+                    None => {
+                        let meta = entry.metadata().expect("fail to read meta");
+                        let created = time::OffsetDateTime::from(
+                            meta.created().expect("fail to get created time"),
+                        );
+
+                        let created_str = format!(
+                            "{}-{:02}-{:02}",
+                            created.year(),
+                            created.month() as u8,
+                            created.day()
+                        );
+
+                        db.insert(filename, created_str.clone())
+                            .expect("fail to insert record");
+                        created_str
+                    }
+                };
+                let storage = match dir {
+                    "easy" => &mut report.easy,
+                    "medium" => &mut report.medium,
+                    "hard" => &mut report.hard,
+                    _ => unreachable!(),
+                };
+                storage
+                    .entry(date)
+                    .and_modify(|i| {
+                        *i += 1;
+                    })
+                    .or_insert(1);
+            }
+        }
+    }
+
+    let mut f = File::create("STAT.md").expect("Fail to create stat.md");
+    for dir in TARGET_DIRS {
+        writeln!(f, "{}\n===\n", dir).unwrap();
+        let storage = match dir {
+            "easy" => &report.easy,
+            "medium" => &report.medium,
+            "hard" => &report.hard,
+            _ => unreachable!(),
+        };
+        if storage.is_empty() {
+            writeln!(f, "N/A").unwrap();
+        } else {
+            writeln!(f, "| date | finished |\n| --- | --- |").unwrap();
+            to_report_table(&mut f, storage);
+        }
+        writeln!(f).unwrap();
+    }
+}
+
+fn to_report_table(f: &mut File, map: &BTreeMap<String, i32>) {
+    for (k, v) in map.iter() {
+        writeln!(f, "| {} | {} |", k, v).expect("Fail to write");
+    }
+}
