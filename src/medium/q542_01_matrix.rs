9ae1fc7dc96b84bbd72a0277487631e0d54fbea1
DanSnow <dododavid006@gmail.com>
2024-06-13 09:30:22 +0800

    fix: use dp to fix tle

--- src/medium/q542_01_matrix.rs
+++ src/medium/q542_01_matrix.rs
@@ -7,63 +7,63 @@ struct Solution;
 
 // @lc code=start
 impl Solution {
-    // FIXME: TLE on large test case
     pub fn update_matrix(mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        use std::{
-            cmp::min,
-            collections::{HashSet, VecDeque},
-        };
-        let mut distances = vec![vec![i32::MAX; mat[0].len()]; mat.len()];
+        use std::cmp::min;
+        let width = mat[0].len() as i32;
+        let height = mat.len() as i32;
+        let max_distance = width + height;
 
-        for i in 0..mat.len() {
-            for j in 0..mat[0].len() {
-                if mat[i][j] == 0 {
-                    distances[i][j] = 0;
+        let mut distances = vec![vec![max_distance; mat[0].len()]; mat.len()];
+
+        // fill zero cells to 0
+        for r in 0..mat.len() {
+            for c in 0..mat[0].len() {
+                if mat[r][c] == 0 {
+                    distances[r as usize][c as usize] = 0;
                 }
             }
         }
 
+        // process from top left to bottom right
         for r in 0..mat.len() {
             for c in 0..mat[0].len() {
                 if mat[r][c] == 1 {
                     let r = r as i32;
                     let c = c as i32;
-                    let mut queue = VecDeque::from(vec![
-                        (r - 1, c, 1),
-                        (r + 1, c, 1),
-                        (r, c - 1, 1),
-                        (r, c + 1, 1),
-                    ]);
-                    let mut seen = HashSet::new();
+                    let top = if r > 0 {
+                        distances[(r - 1) as usize][c as usize]
+                    } else {
+                        max_distance
+                    };
+                    let left = if c > 0 {
+                        distances[r as usize][(c - 1) as usize]
+                    } else {
+                        max_distance
+                    };
+                    distances[r as usize][c as usize] =
+                        min(min(left, top) + 1, distances[r as usize][c as usize]);
+                }
+            }
+        }
 
-                    let current_row = r as usize;
-                    let current_col = c as usize;
-                    // bfs to find the shortest path
-                    while let Some((r, c, d)) = queue.pop_front() {
-                        if seen.contains(&(r, c)) {
-                            continue;
-                        }
-                        seen.insert((r, c));
-
-                        // it's obviously not possible to have a shorter path
-                        if distances[current_row][current_col] < d {
-                            continue;
-                        }
-
-                        if r >= 0 && r < mat.len() as i32 && c >= 0 && c < mat[0].len() as i32 {
-                            if distances[r as usize][c as usize] == i32::MAX {
-                                queue.push_back((r + 1, c, d + 1));
-                                queue.push_back((r - 1, c, d + 1));
-                                queue.push_back((r, c + 1, d + 1));
-                                queue.push_back((r, c - 1, d + 1));
-                            } else {
-                                distances[current_row][current_col] = min(
-                                    distances[r as usize][c as usize].saturating_add(d),
-                                    distances[current_row][current_col],
-                                );
-                            }
-                        }
-                    }
+        // process from bottom right to top left
+        for r in (0..mat.len()).rev() {
+            for c in (0..mat[0].len()).rev() {
+                if mat[r][c] == 1 {
+                    let r = r as i32;
+                    let c = c as i32;
+                    let bottom = if r < height - 1 {
+                        distances[(r + 1) as usize][c as usize]
+                    } else {
+                        max_distance
+                    };
+                    let right = if c < width - 1 {
+                        distances[r as usize][(c + 1) as usize]
+                    } else {
+                        max_distance
+                    };
+                    distances[r as usize][c as usize] =
+                        min(min(bottom, right) + 1, distances[r as usize][c as usize]);
                 }
             }
         }
