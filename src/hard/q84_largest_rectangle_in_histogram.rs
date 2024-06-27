97c773f992eb3000b66b932e6119f23135cbc318
DanSnow <dododavid006@gmail.com>
2024-06-27 18:15:33 +0800

    fix: grind 75 w8 q84

--- src/hard/q84_largest_rectangle_in_histogram.rs
+++ src/hard/q84_largest_rectangle_in_histogram.rs
@@ -8,37 +8,73 @@ struct Solution;
 
 // @lc code=start
 impl Solution {
+    // ref: https://medium.com/algorithms-digest/largest-rectangle-in-histogram-234004ecd15a
     pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
-        use std::cmp::{max, min};
+        use std::cmp::max;
         if heights.is_empty() {
             return 0;
         }
-        let mut current_height = heights[0];
-        let mut max_rectangle = heights[0];
-        let mut width = 1;
 
-        for height in heights[1..].iter().copied() {
-            current_height = min(height, current_height);
-            let rectangle_with_current_bar = (width + 1) * current_height;
-            let rectangle_from_scratch = 1 * height;
-            dbg!(
-                current_height,
-                height,
-                width,
-                rectangle_from_scratch,
-                rectangle_with_current_bar
-            );
-            if rectangle_from_scratch >= rectangle_with_current_bar {
-                width = 1;
-                max_rectangle = max(max_rectangle, rectangle_from_scratch);
-                current_height = height;
-            } else {
-                width += 1;
-                max_rectangle = max(rectangle_with_current_bar, max_rectangle);
-            }
+        #[derive(Debug, Clone, Copy, Eq, PartialEq)]
+        struct Bar {
+            height: i32,
+            index: i32,
         }
 
-        max_rectangle
+        // the idea here is we need to find the previous smaller element from the left side
+        // so we can use previous element's index + 1 to make the largest area until current position
+        // ref: https://medium.com/algorithms-digest/previous-smaller-element-e3996fb8be3c
+
+        // we add a extra element to ensure that we can correctly handle the last item.
+        let mut left_side_smaller_elements: Vec<Bar> = vec![Bar {
+            height: -1,
+            index: -1,
+        }];
+
+        let mut max_area = heights[0];
+
+        for (i, h) in heights.iter().copied().enumerate() {
+            let i = i as i32;
+            // pop all bars that are taller than current bar
+            // we don't need to check the empty as the `let` also check for it
+            while let Some(Bar { height, .. }) = left_side_smaller_elements.last().copied() {
+                if height < h {
+                    break;
+                }
+
+                // pop the bar
+                let Bar { height, .. } = left_side_smaller_elements.pop().unwrap();
+
+                // calculate the area with "i" as the right boundary (exclusive) of the bar
+                let width = i
+                    - left_side_smaller_elements
+                        .last()
+                        .map(|b| b.index)
+                        .unwrap_or(0)
+                    - 1;
+                max_area = max(max_area, width * height);
+            }
+            left_side_smaller_elements.push(Bar {
+                index: i,
+                height: h,
+            });
+        }
+
+        // pop the remaining bars
+        // now we have the smaller elements from the left side
+        // which mean all the element should be safe to extend to right side
+        while let Some(Bar { height, .. }) = left_side_smaller_elements.pop() {
+            // need to find the previous one to create the largest area
+            let width = heights.len() as i32
+                - left_side_smaller_elements
+                    .last()
+                    .map(|b| b.index)
+                    .unwrap_or(0)
+                - 1;
+            max_area = max_area.max(width * height);
+        }
+
+        max_area
     }
 }
 // @lc code=end
@@ -49,7 +85,8 @@ mod tests {
 
     #[test]
     fn it_work() {
-        // assert_eq!(10, Solution::largest_rectangle_area(vec![2, 1, 5, 6, 2, 3]));
+        assert_eq!(10, Solution::largest_rectangle_area(vec![2, 1, 5, 6, 2, 3]));
         assert_eq!(9, Solution::largest_rectangle_area(vec![1, 2, 3, 4, 5]));
+        assert_eq!(2, Solution::largest_rectangle_area(vec![1, 1]));
     }
 }
