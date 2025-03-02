7c3f83793b791a18412fa32fe2422da202c626c5
DanSnow <dododavid006@gmail.com>
2025-03-02 12:15:55 +0800

    pref(2342): optimize with vector

--- src/medium/q2342_maximum_sum.rs
+++ src/medium/q2342_maximum_sum.rs
@@ -1,5 +1,5 @@
 pub fn maximum_sum(nums: Vec<i32>) -> i32 {
-    use std::{cmp, collections::HashMap};
+    use std::cmp;
 
     fn digit_sum(mut num: i32) -> i32 {
         let mut sum = 0;
@@ -27,18 +27,16 @@ pub fn maximum_sum(nums: Vec<i32>) -> i32 {
 
     let mut max = -1;
 
-    let mut map = HashMap::new();
+    // every num is less than 10^9, so the number with max sum will be `99999999` => 81
+    let mut map = vec![-1; 82];
 
     for num in nums.iter().copied() {
         let num = Num::new(num);
-        map.entry(num.sum)
-            .and_modify(|origin: &mut Num| {
-                max = cmp::max(max, origin.num + num.num);
-                if origin.num < num.num {
-                    *origin = num;
-                }
-            })
-            .or_insert(num);
+        let index = num.sum as usize;
+        if map[index] != -1 {
+            max = cmp::max(max, map[index] + num.num);
+        }
+        map[index] = cmp::max(map[index], num.num);
     }
 
     max
